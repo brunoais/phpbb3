4a9b199071e5ff17b5f879ee92bd74040e790c0d
Bruno Ais <brunoaiss@gmail.com>
2012-12-18 16:07:22 +0000

    [feature/bbcode-engine-v2] Improvements to the simple tester
    Made some changes to the simple tester of the BBCode system
    such that it now is testing lists and [*], specially the auto-
    closing of the [*] that are left open.
    
    PHPBB3-9795

--- phpBB/bbcode_parser_test.php
+++ phpBB/bbcode_parser_test.php
@@ -1,11 +1,14 @@
 <?php
-	
-	require "includes/bbcode/phpbb_bold_parser.php";
-	require "includes/bbcode/phpbb_italic_parser.php";
-	require "includes/bbcode/phpbb_underline_parser.php";
-	require "includes/bbcode/phpbb_list_parser.php";
-	
-	require "includes/bbcode/phpbb_bbcode_parser.php";
+
+define('IN_PHPBB', true);
+
+require "includes/bbcode/bold_parser.php";
+require "includes/bbcode/italic_parser.php";
+require "includes/bbcode/underline_parser.php";
+require "includes/bbcode/list_parser.php";
+require "includes/bbcode/ 42_parser.php";
+
+require "includes/bbcode/bbcode_parser.php";
 	
 	// $string =
 // '[/abc][abc=aij] [/ubc][ubc][ab]a [abc=badOverride][abc="I got a \"child\"!"][ubc rightBoss="true"] YAY[/ubc] [/abc][/ubc][/abc][abc param1="it is \"val1\"" param2="it is \"val2\"" ] [/ubc][/abc][/ab]b [abc][/abc] c[/ubc][ubc][/ubc][/ubc][/ubc][ubc]';
@@ -32,44 +35,68 @@
 		[/ubc]
 	[/abc]';
 	
-	$string = '
-	[b child="0"]
-		[b child="0,0"] 
-		boldy!
-		[/b]
-		[u child="0,1"]
-			[b child="0,1,0"] 
-				[u child="0,1,0,0"] 
-					underlined bold
-				[/u]
-			[/b]
-		[/u]
-	[/b]
-	[b child="1"]
-	another bold
-		[b child="1,0"]
-			[u child="1,0,0"] 
-			another underlined bold
-			[/u]
-		[/b]
-		[u child="1,1"]
-			[b child="1,1,0"] 
-			[/b]
-		[/u]
-	[/b]';
+	// $string = '
+	// [b child="0"]
+		// [b child="0,0"] 
+		// boldy!
+		// [/b]
+		// [u child="0,1"]
+			// [b child="0,1,0"] 
+				// [u child="0,1,0,0"] 
+					// underlined bold
+				// [/u]
+			// [/b]
+		// [/u]
+	// [/b]
+	// [b child="1"]
+	// another bold
+		// [b child="1,0"]
+			// [u child="1,0,0"] 
+			// another underlined bold
+			// [/u]
+		// [/b]
+		// [u child="1,1"]
+			// [b child="1,1,0"] 
+			// [/b]
+		// [/u]
+	// [/b]';
 	
 	$string = '
-	[b child="0"]
-		[u child="0,1"]
-			[b child="0,1,0"] 
-			[/b]
-		[/u]
-	[/b]
-	[b child="1"]
-	another bold
-		[b child="1,0"]
-		[/b]
-	[/b]';
+[list]
+WTFelem
+[*]elem1
+[*][list][*]elem2 [/list]
+[*]elem3
+[/list]
+	[b]ending[/b]';
+$string = '
+[list]
+	[*]elem1
+	[*]
+	[list]
+		[*][b]elem2[/b]
+		[*]
+		[list]
+			[*]yeah
+			[*][*]yep
+		[/list]
+	[/list]
+	[*]elem3
+[/list]
+	[b]ending[/b]';
+	
+	// $string = '
+	// [b child="0"]
+		// [u child="0,1"]
+			// [b child="0,1,0"] 
+			// [/b]
+		// [/u]
+	// [/b]
+	// [b child="1"]
+	// another bold
+		// [b child="1,0"]
+		// [/b]
+	// [/b]';
 	
 	// $string = '';
 	
@@ -77,15 +104,23 @@
 	$bbcode_tags = array(
 				'b' => array(
 							'flags'		=> 0,
-							'callback'	=> new phpbb_bold_parser()
+							'callback'	=> new phpbb_bbcode_bold_parser()
 							),
 				'i' => array(
 							'flags'		=> 0,
-							'callback'	=> new phpbb_italic_parser()
+							'callback'	=> new phpbb_bbcode_italic_parser()
 							),
 				'u' => array(
 							'flags'		=> 0,
-							'callback'	=> new phpbb_underline_parser()
+							'callback'	=> new phpbb_bbcode_underline_parser()
+							),
+				'list' => array(
+							'flags'		=> 0,
+							'callback'	=> new phpbb_bbcode_list_parser()
+							),
+				'*' => array(
+							'flags'		=> 0,
+							'callback'	=> new phpbb_bbcode_¸42_parser()
 							),
 				);
 
@@ -93,21 +128,22 @@
 
 
 $start = microtime(true);
-$parser = new phpbb_bbcode_parser($string, $bbcode_tags);
+$parser = new phpbb_bbcode_bbcode_parser($string, $bbcode_tags);
 
 // $final_string = $parser->parse();
 $final_string = $parser->parse_phase1();
 
 $end = microtime(true);
 
-var_dump($string, $final_string);
-echo "<br><br>";
+// var_dump($string, $final_string);
+// echo "<br><br>";
 var_dump($end - $start);
 
 $start2 = microtime(true);
 $final_string = $parser->parse_phase2();
 $end2 = microtime(true);
-echo "<br><br>";
+// echo "<br><br>";
 var_dump($final_string);
-echo "<br><br>";
-var_dump($end2 - $start2);
+// echo "<br><br>";
+var_dump($end2 - $start);
+// var_dump(0.00005);
